04ca989c29a12214561c6d3d03275c8164d1219d
Kumar Murthy <bharathmurthy1992@gmail.com>
2025-02-11 14:45:47 +1000

    Added a Intercom chatbot to answer questions about myself

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { useEffect } from 'react';
 import Portfolio from './components/Portfolio';
 import ReactGA from 'react-ga4';
+import Intercom from '@intercom/messenger-js-sdk';
 
 function App() {
+  Intercom({
+    app_id: 'dn4szgb4',
+  });
+  
   useEffect(() => {
     ReactGA.send("pageview");
   }, []);
